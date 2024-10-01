73fe810e65d9e29c7df39ea290eb6884e342f1f4
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2024-10-01 13:57:58 -0400

    sdfl['jed

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const DOM={
-    button:document.getElementById("btn"),
-    text:document.getElementById("text"),
-    
-
+let student={
+    name:"ryan",
+    age:15,
+    gf:true
+};
+const header = document.querySelector("h1")
+console.log(student.age)
+const DOMSelectors={
+    header:document.querySelector("h1"),
+    card:document.querySelector(".card"),
+    items:document.querySelectorAll("li"),
+    cardHeader:document.querySelectorAll(".card-header"),
+    button:document.querySelector(".btn"),
+    form:document.querySelector("form")
 }
+console.log(DOMSelectors.card);
+
+DOMSelectors.button.addEventListener("click",function(event){
+    console.log(event.target);
+    event.target.parentElement.style.backgroundColor="red";
+})
+DOMSelectors.form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    console.log(document.querySelector("input").value);
+})
