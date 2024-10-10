ab4c2a625052fe8a8ff23cb13693286328014aa5
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2024-10-10 13:56:47 -0400

    tguo

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-let student={
-    name:"ryan",
-    age:15,
-    gf:true
-};
-const header = document.querySelector("h1")
-console.log(student.age)
-const DOMSelectors={
-    header:document.querySelector("h1"),
-    card:document.querySelector(".card"),
-    items:document.querySelectorAll("li"),
-    cardHeader:document.querySelectorAll(".card-header"),
-    button:document.querySelector(".btn"),
-    form:document.querySelector("form"),
+const Selectors = {
+    formImg:document.getElementById("img"),
 }
-console.log(DOMSelectors.card);
 
 
-DOMSelectors.form.addEventListener("submit",function(event){
+
+Selectors.formImg.addEventListener("submit",function(event){
     event.preventDefault();
-    let input = document.querySelector("input");
-    console.log(input.value)
-    input.value = "";
-    console.log(input.value)
-
-    
-
+    Selectors.container.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card"><h2 class="card-header"><img src="${Selectors.formImg}" alt=""></h2></div>`
+      );
 })
 
+  
