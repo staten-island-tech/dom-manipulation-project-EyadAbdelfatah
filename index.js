cd14e1511d554d12419a0a1b225c76f5f8ddd5ec
mellowed24 <eyada5@nycstudents.net>
2024-10-11 13:11:04 -0400

    jgcujlk

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const Selectors = {
-    formImg:document.getElementById("img"),
-}
+const DOMSelectors = {
+  container: document.querySelector(".container"),
+  imgInput: document.querySelector("#img-input"), 
+  formImg: document.querySelector("#img") 
+};
+
+DOMSelectors.formImg.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  const imgUrl = DOMSelectors.imgInput.value; 
 
 
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card">
+      <h2 class="card-header">
+        <img src="${imgUrl}" alt="User Image" style="width: 100%; height: auto;">
+      </h2>
+    </div>`
+  );
 
-Selectors.formImg.addEventListener("submit",function(event){
-    event.preventDefault();
-    Selectors.container.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card"><h2 class="card-header"><img src="${Selectors.formImg}" alt=""></h2></div>`
-      );
-})
-
-  
+  DOMSelectors.imgInput.value = "";
+});
