addaa448f4aa1bc6d1444858add5c812aa2a66ab
mellowed24 <eyada5@nycstudents.net>
2024-10-15 20:18:45 -0400

    k

--- index.js
+++ index.js
@@ -4,42 +4,39 @@ const DOMSelectors = {
   button: document.querySelector(".btn"),
   nameInput: document.querySelector("#name-input"),
   form: document.querySelector("form"),
-  removeButton: document.querySelector("#remove"),
+  removeButton:document.querySelector("#remove")
 };
-const imgUrl = DOMSelectors.imgInput.value;
-const name = DOMSelectors.nameInput.value;
-function createCard() {
+
+function createCard(event) {
   event.preventDefault();
+
+  const imgUrl = DOMSelectors.imgInput.value;
+  const name = DOMSelectors.nameInput.value;
 
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `
     <div class="card" >
     <h2 class="card-header"> ${name} </h2>
-        <img src="${imgUrl}" alt="User Image" id = "card img">
+        <img src="${imgUrl}" alt="User Image" class = "card-img">
     </div>
     <button type="submit" class="btn" id = "remove">Remove</button> `
+
   );
 
   
 }
-function clearCard(){
+function removeCard(event){
+  event.preventDefault();
+  document.querySelector(".card").remove();
+  
+}
+function clearInput(){
+  DOMSelectors.imgInput.value = "";
+  DOMSelectors.nameInput.value = "";
   imgUrl = "";
   name = "";
-
 }
-function removeCard() {
-  event.preventDefault();
-  DOMSelectors.container.insertAdjacentHTML(
-    "beforeend",
-    `
-    <div class="card" >
 
-    </div>`
-  );
-}
-DOMSelectors.form.addEventListener("submit", function(event){
-  createCard();
-  clearCard();
-})
-DOMSelectors.removeButton.addEventListener("click", removeCard);
+DOMSelectors.form.addEventListener("submit", createCard );
+DOMSelectors.removeButton.addEventListener("click", removeCard );
